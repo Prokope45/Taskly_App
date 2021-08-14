b18ce3dcf63a7295d5163dae71484457f60a9452
Jared <Paubelj45@gmail.com>
2021-08-14 11:32:59 -0500

    Added user registration; automated username generation.

--- src/users/serializers.py
+++ src/users/serializers.py
@@ -4,6 +4,21 @@ from rest_framework import serializers
 
 class UserSerializer(serializers.ModelSerializer):
 
+    password = serializers.CharField(write_only=True, required=False)
+    username = serializers.CharField(read_only=True)
+
+    def create(self, validated_data):
+        password = validated_data.pop('password')
+        user = User.objects.create(**validated_data) # creates new user objects using
+                                                     # validated_data fields
+        user.set_password(password)
+        user.save()
+
+        return user
+
     class Meta:
         model = User
-        fields = ['url', 'id', 'username', 'email', 'first_name', 'last_name', 'email']
+        fields = ['url', 'id', 'username', 'email',
+                  'first_name', 'last_name', 'email',
+                  'password'
+                  ]
--- src/users/signals.py
+++ src/users/signals.py
@@ -7,3 +7,13 @@ from .models import Profile
 def create_user_profile(sender, instance, created, **kwargs):
     if created:
         Profile.objects.create(user=instance)
+
+@receiver(pre_save, sender=User)
+def set_username(sender, instance, **kwargs):
+    if not instance.username:
+        username = f'{instance.first_name}_{instance.last_name}'.lower()
+        counter = 1
+        while User.objects.filter(username=username):
+            username = f'{instance.first_name}_{instance.last_name}_{counter}'.lower()
+            counter += 1
+        instance.username = username
