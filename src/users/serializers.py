1721c104f33809dfea420e53ec6495bc84014370
Jared <Paubelj45@gmail.com>
2021-09-11 14:22:17 -0500

    Tested token based authentication as well as created a change password functionality.

--- src/users/serializers.py
+++ src/users/serializers.py
@@ -2,9 +2,11 @@ from django.contrib.auth.models import User
 
 from rest_framework import serializers
 
+
 class UserSerializer(serializers.ModelSerializer):
 
     password = serializers.CharField(write_only=True, required=False)
+    old_password = serializers.CharField(write_only=True, required=False)
     username = serializers.CharField(read_only=True)
 
     def create(self, validated_data):
@@ -16,9 +18,23 @@ class UserSerializer(serializers.ModelSerializer):
 
         return user
 
+    def update(self, instance, validated_data):
+        try:
+            user = instance
+            password = validated_data.pop('password')
+            old_password = validated_data.pop('old_password')
+            if user.check_password(old_password):
+                user.set_password(password)
+            else:
+                raise Exception("Old password is incorrect.")
+            user.save()
+        except Exception as error:
+            raise serializers.ValidationError({'info': error})
+        return super(UserSerializer, self).update(instance, validated_data)
+
     class Meta:
         model = User
         fields = ['url', 'id', 'username', 'email',
                   'first_name', 'last_name', 'email',
-                  'password'
+                  'password', 'old_password'
                   ]
