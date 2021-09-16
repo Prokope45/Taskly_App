9aca5f6eba143599eb824343859d233aee829998
Jared <synetos@protonmail.com>
2021-09-16 18:20:13 -0500

    Made user memberships where profiles can be assigned to a house whether they are managers or users.

--- src/house/serializers.py
+++ src/house/serializers.py
@@ -5,10 +5,12 @@ from .models import House
 
 class HouseSerializer(serializers.ModelSerializer):
     members_count = serializers.IntegerField(read_only=True)
+    members = serializers.HyperlinkedRelatedField(read_only=True, many=True, view_name='profile-detail')
+    manager = serializers.HyperlinkedRelatedField(read_only=True, many=False, view_name='profile-detail')
 
     class Meta:
         model = House
         fields = ['url', 'id', 'image', 'name', 'created_on',
-                  'manager', 'description', 'members_count', 'points',
-                  'completed_tasks_count', 'not_completed_tasks_count']
+                  'manager', 'description', 'members_count', 'members',
+                  'points', 'completed_tasks_count', 'not_completed_tasks_count']
         read_only_fields = ['points', 'completed_tasks_count', 'not_completed_tasks_count']
--- src/users/models.py
+++ src/users/models.py
@@ -4,6 +4,7 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.utils.deconstruct import deconstructible
 
+
 @deconstructible
 class GenerateProfileImagePath(object):
 
@@ -16,6 +17,7 @@ class GenerateProfileImagePath(object):
         name = f'profile_image.{ext}'
         return os.path.join(path, name)
 
+
 user_profile_image_path = GenerateProfileImagePath()
 
 
@@ -23,6 +25,8 @@ class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     image = models.FileField(upload_to=user_profile_image_path,
                              blank=True, null=True)
+    house = models.ForeignKey('house.House', on_delete=models.SET_NULL,
+                              null=True, blank=True, related_name='members')
 
     def __str__(self):
         return f'{self.user.username.capitalize()}\'s Profile'
