08e1506cac75205602fcde567ff11e0b5a898a39
Jared <synetos@protonmail.com>
2021-09-25 12:11:16 -0500

    Added the ability to remove members for managers of houses and for admins. Testing shows that it works as intended as well.

--- src/house/viewsets.py
+++ src/house/viewsets.py
@@ -1,3 +1,5 @@
+from django.contrib.auth.models import User
+
 from rest_framework import viewsets, status
 from rest_framework.decorators import action
 from rest_framework.response import Response
@@ -42,3 +44,23 @@ class HouseViewSet(viewsets.ModelViewSet):
                 return Response({'detail': 'User not a member in this house.'}, status=status.HTTP_400_BAD_REQUEST)
         except Exception as e:
             return Response(status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+    @action(detail=True, methods=['post'], name='Remove Member')
+    def remove_member(self, request, pk=None):
+        try:
+            house = self.get_object()
+            user_id = request.data.get('user_id', None)
+            if user_id is None:
+                return Response({'user_id': 'Not provided.'}, status=status.HTTP_400_BAD_REQUEST)
+
+            user_profile = User.objects.get(pk=user_id).profile
+            house_members = house.members
+            if user_profile in house_members.all():
+                house_members.remove(user_profile)
+                house.save()
+                return Response(status=status.HTTP_204_NO_CONTENT)
+            else:
+                return Response({'detail': 'User is not a member in this house.'}, status=status.HTTP_400_BAD_REQUEST)
+
+        except User.DoesNotExist as e:
+            return Response({'detail': 'Provided user_id does not exist.'}, status=status.HTTP_400_BAD_REQUEST)
