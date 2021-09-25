968fc637bd2704c221b96be014e2d8216aea5975
Jared <synetos@protonmail.com>
2021-09-25 12:24:09 -0500

    Added the ability for managers to upload images to the house instances.

--- src/taskful_api/settings.py
+++ src/taskful_api/settings.py
@@ -62,6 +62,7 @@ TEMPLATES = [
             'context_processors': [
                 'django.template.context_processors.debug',
                 'django.template.context_processors.request',
+                'django.template.context_processors.media',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
                 'social_django.context_processors.backends',
@@ -141,3 +142,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+MEDIA_URL = '/media/'
--- src/taskful_api/urls.py
+++ src/taskful_api/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 from django.conf import settings
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.static import static
 
 from users import router as user_api_router
 from house import router as house_api_router
@@ -37,3 +38,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include(api_url_patterns))
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
