d4e198654e4a0ea559a27963ddaade3add204f02
Jared <synetos@protonmail.com>
2021-09-26 14:12:10 -0500

    Added task list model in task app with a file pathway for the attachment upload option.

--- src/task/models.py
+++ src/task/models.py
@@ -1,4 +1,7 @@
+import uuid, os
+
 from django.db import models
+from django.utils.deconstruct import deconstructible
 
 NOT_COMPLETE = 'NC'
 COMPLETE = 'C'
@@ -7,14 +10,27 @@ TASK_STATUS_CHOICES = [
     (COMPLETE, 'Complete'),
 ]
 
+@deconstructible
+class GenerateAttachmentFile(object):
 
-class Task(models.Model):
+    def __init__(self):
+        pass
+
+    def __call__(self, instance, filename):
+        ext = filename.split('.') [-1]
+        path = f'media/task/{instance.task.id}/attachments'
+        name = f'{instance.id}.{ext}'
+        return os.path.join(path, name)
+
+attachment_file_path = GenerateAttachmentFile()
+
+
+class TaskList(models.Model):
     created_on = models.DateTimeField(auto_now_add=True)
     completed_on = models.DateTimeField(null=True, blank=False)
     created_by = models.ForeignKey('users.Profile', null=True, blank=True,
-                                   on_delete=models.SET_NULL, related_name='created_tasks')
-    completed_by = models.ForeignKey('users.Profile', null=True, blank=True,
-                                     on_delete=models.SET_NULL, related_name='completed_tasks')
+                                   on_delete=models.SET_NULL, related_name='lists')
+    house = models.ForeignKey('house.House', on_delete=models.CASCADE, related_name='lists')
     name = models.CharField(max_length=120)
     description = models.TextField(null=True, blank=True)
     status = models.CharField(
@@ -25,3 +41,33 @@ class Task(models.Model):
 
     def __str__(self):
         return f'{self.id} | {self.name}'
+
+
+class Task(models.Model):
+    created_on = models.DateTimeField(auto_now_add=True)
+    completed_on = models.DateTimeField(null=True, blank=False)
+    created_by = models.ForeignKey('users.Profile', null=True, blank=True,
+                                   on_delete=models.SET_NULL, related_name='created_tasks')
+    completed_by = models.ForeignKey('users.Profile', null=True, blank=True,
+                                     on_delete=models.SET_NULL, related_name='completed_tasks')
+    task_list = models.ForeignKey('task.TaskList', on_delete=models.CASCADE, related_name='tasks')
+    name = models.CharField(max_length=120)
+    description = models.TextField(null=True, blank=True)
+    status = models.CharField(
+        max_length=2,
+        choices=TASK_STATUS_CHOICES,
+        default=NOT_COMPLETE
+    )
+
+    def __str__(self):
+        return f'{self.id} | {self.name}'
+
+
+class Attachment(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    created_on = models.DateTimeField(auto_now_add=True)
+    data = models.FileField(upload_to=attachment_file_path)
+    task = models.ForeignKey('task.Task', on_delete=models.CASCADE, related_name='attachments')
+
+    def __str__(self):
+        return f'{self.id} | {self.task}'
