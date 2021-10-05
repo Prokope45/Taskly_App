9b53e4ee7475160b9618fd1fa31a130497186740
JaredP45 <synetos@protonmail.com>
2021-10-05 16:19:43 -0500

    Created Task and Attachment viewsets with corresponding permissions and linked serializers to each viewset.

--- src/task/permissions.py
+++ src/task/permissions.py
@@ -18,3 +18,33 @@ class IsAllowedEditTaskListElseNone(permissions.BasePermission):
 
     def has_object_permission(self, request, view, obj):
         return request.user.profile == obj.created_by
+
+
+class IsAllowedToEditTaskElseNone(permissions.BasePermission):
+    """
+    Custom permissions for the TaskListViewSet to only allow members to have access to the tasks assigned
+    """
+
+    def has_permission(self, request, view):
+
+        if not request.user.is_anonymous:
+            return request.user.profile.house is not None
+
+        return False
+
+    def has_object_permission(self, request, view, obj):
+
+        return request.user.profile.house is obj.task_list.house
+
+
+class IsAllowedToEditAttchmentElseNone(permissions.BasePermission):
+
+    def has_permission(self, request, view):
+        if not request.user.is_anonymous:
+            return request.user.profile.house is not None
+
+        return False
+
+    def has_object_permission(self, request, view, obj):
+
+        return request.user.profile.house is obj.task.task_list.house
--- src/task/viewsets.py
+++ src/task/viewsets.py
@@ -1,7 +1,7 @@
 from rest_framework import viewsets, mixins
-from .serializers import TaskListSerializer
+from .serializers import TaskListSerializer, TaskSerializer, AttachmentSerializer
 from .models import Task, TaskList, Attachment
-from .permissions import IsAllowedEditTaskListElseNone
+from .permissions import IsAllowedEditTaskListElseNone, IsAllowedToEditTaskElseNone, IsAllowedToEditAttchmentElseNone
 
 
 class TaskListViewSet(mixins.CreateModelMixin,
@@ -14,3 +14,20 @@ class TaskListViewSet(mixins.CreateModelMixin,
     permission_classes = [IsAllowedEditTaskListElseNone]
     queryset = TaskList.objects.all()
     serializer_class = TaskListSerializer
+
+
+class TaskViewSet(viewsets.ModelViewSet):
+    permission_classes = [IsAllowedToEditTaskElseNone]
+    queryset = Task.objects.all()
+    serializer_class = TaskSerializer
+
+
+class AttachmentViewSet(mixins.CreateModelMixin,
+                        mixins.RetrieveModelMixin,
+                        mixins.UpdateModelMixin,
+                        mixins.DestroyModelMixin,
+                        mixins.ListModelMixin,
+                        viewsets.GenericViewSet):
+    permission_classes = [IsAllowedToEditAttchmentElseNone]
+    queryset =  Attachment.objects.all()
+    serializer_class = AttachmentSerializer
