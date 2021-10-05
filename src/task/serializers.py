f8dd85cdf0b1f480a7c3877d68141c9559874fa4
JaredP45 <synetos@protonmail.com>
2021-10-05 16:06:34 -0500

    Created serializers for Task and Attachment viewsets.

--- src/task/serializers.py
+++ src/task/serializers.py
@@ -13,3 +13,27 @@ class TaskListSerializer(serializers.ModelSerializer):
         fields = ['url', 'id', 'name', 'description', 'status',
                   'created_on', 'created_by', 'house']
         read_only_fields = ['created_on', 'status']
+
+
+class TaskSerializer(serializers.ModelSerializer):
+
+    created_by = serializers.HyperlinkedRelatedField(read_only=True, many=False, view_name='profile-detail')
+    completed_by = serializers.HyperlinkedRelatedField(read_only=True, many=False, view_name='profile-detail')
+    task_list = serializers.HyperlinkedRelatedField(queryset=TaskList.objects.all(), many=False,
+                                                    view_name='tasklist-detail')
+
+    class Meta:
+        model = Task
+        fields = ['url', 'id', 'name', 'description', 'status', 'created_on', 'completed_on',
+                  'created_by', 'completed_by', 'task_list']
+        read_only_fields = ['created_on', 'created_by', 'completed_on', 'completed_by']
+
+
+class AttachmentSerializer(serializers.ModelSerializer):
+
+    task = serializers.HyperlinkedRelatedField(queryset=Task.objects.all(), many=False, view_name='task-detail')
+
+    class Meta:
+        model = Attachment
+        fields = ['url', 'id', 'created_on', 'data', 'task']
+        read_only_fields = ['created_on']
