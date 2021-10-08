6f83aaef6299095c769eee6e605d1f07202d8508
JaredP45 <synetos@protonmail.com>
2021-10-08 11:03:16 -0500

    Created signals for updating the database whether a task is complete or incomplete, and implemented point system to completed tasks and incomplete tasks.

--- src/task/permissions.py
+++ src/task/permissions.py
@@ -28,7 +28,7 @@ class IsAllowedToEditTaskElseNone(permissions.BasePermission):
     def has_permission(self, request, view):
 
         if not request.user.is_anonymous:
-            return request.user.profile.house is not None
+            return request.user.profile.house != None
 
         return False
 
@@ -47,4 +47,4 @@ class IsAllowedToEditAttachmentElseNone(permissions.BasePermission):
 
     def has_object_permission(self, request, view, obj):
 
-        return request.user.profile.house == obj.task.task_list.house
+        return request.user.profile.house == obj.task_list.house
