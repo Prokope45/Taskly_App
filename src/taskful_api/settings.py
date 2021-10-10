ed0dbe50f81aa63078b47607a159281e8e353211
JaredP45 <synetos@protonmail.com>
2021-10-10 15:01:28 -0500

    Created PostgreSQL database connection with Heroku server.

--- src/taskful_api/settings.py
+++ src/taskful_api/settings.py
@@ -85,8 +85,12 @@ WSGI_APPLICATION = 'taskful_api.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': config('DATABASE_NAME'),
+        'USER': config('DATABASE_USER'),
+        'PASSWORD': config('DATABASE_PASSWORD'),
+        'HOST': config('DATABASE_HOST'),
+        'PORT': config('DATABASE_PORT'),
     }
 }
 
